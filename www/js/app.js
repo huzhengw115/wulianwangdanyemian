c0d2c2498bc0c0316a93d65882622403b60a74ac
huzhengwei <745602138@qq.com>
2016-12-06 14:20:17 +0800

    20161206

--- www/js/app.js
+++ www/js/app.js
@@ -63,3 +63,72 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/news')
 })
+
+.directive('hideTabs', function ($rootScope) {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attributes) {
+      scope.$on('$ionicView.beforeEnter', function () {
+        scope.$watch(attributes.hideTabs, function (value){
+          $rootScope.hideTabs = value
+        })
+      })
+      scope.$on('$ionicView.beforeLeave', function () {
+        $rootScope.hideTabs = false
+      })
+    }
+  }
+})
+
+.service('swipeService', function ($document) {
+  var photoswipe = function () {
+    var items = []
+    var pswpElement = document.querySelectorAll('.pswp')[0]
+    var options = {
+      index: 0,
+      shareEl: false,
+      tapToClose: true  
+    }
+    function imgOnload(img, index) {
+      return function () {
+        // console.log("imgOnload: [index, img]", index, img);
+        // 只有实际图片宽度大于250时，才需要放大图片，否则可能时图标或者无需放大的图片。
+        if (img.naturalWidth > 250) {
+          var item = {
+            src: img.src,
+            w: img.naturalWidth,
+            h: img.naturalHeight
+          }
+          items.push(item)
+          angular.element(img).bind("click", bindImg(index))
+        }
+      }
+    }
+    function bindImg(index) {
+      return function () {
+        console.log("bindImg: [index, items]", index, items)
+        options.index = index
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options)
+        gallery.init()
+        gallery.goTo(index)
+      }
+    }
+    var databigpic = angular.element($document.find("main"))
+    databigpic.ready(function () {
+      var imgs = databigpic.find("img")
+      console.log('imgs:',imgs)
+      for(var index = 0; index < imgs.length; ++index) {
+        var img = imgs[index]
+        if(img.naturalWidth == 0 && img.naturalHeight == 0) {
+          // 绑定图片加载完成事件，加载完以后才能获取图片Size
+          angular.element(img).bind("load", imgOnload(img, index))
+        } else {
+          imgOnload(img, index)();
+        }
+      }
+    })
+  }
+  return{
+    photoswipe: photoswipe
+  }
+})
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('NewsCtrl', function ($scope, newsService) {
+.controller('NewsCtrl', function ($scope, newsService, swipeService) {
 	var getNewsData = function () {
 		newsService.getNewsData().then(function (newsData) {
 			$scope.newsData = newsData
 			console.log('$scope.newsData:',$scope.newsData)
+			swipeService.photoswipe()
+			if($scope.newsData.pic == '') {
+				$scope.isPicShow = false
+			}else {
+				$scope.isPicShow = true
+			}
 		})
 	}
 	getNewsData()
@@ -12,15 +18,16 @@ angular.module('starter.controllers', [])
 
 .controller('ProjectCtrl', function ($scope, newsService) {
 	$scope.projectData = []
+	$scope.openButton = true
 	var getProjectData = function () {
 		newsService.getProjectData().then(function (projectData) {
 			$scope.projectData = projectData
 			console.log('$scope.projectData:',$scope.projectData)
+			// swipeService.photoswipe()
 		})
 	}
 	$scope.showTable = function (data) {
 		$scope.isShow = true
-		console.log(data)
 		$scope.tableShow = data
 		console.log($scope.tableShow)
 	}
@@ -29,6 +36,8 @@ angular.module('starter.controllers', [])
 	}
 	$scope.toBeLong = function () {
 		$scope.projectTable = !$scope.projectTable
+		$scope.openButton = !$scope.openButton
+		$scope.closeButton = !$scope.closeButton
 	}
 	getProjectData()
 })
